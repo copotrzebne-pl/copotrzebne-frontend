7fb5e173f80df2f2954f79e15f2b85010ceb1db4
Agnieszka Lasyk <agnieszka.lasyk@schibsted.pl>
2022-03-21 23:52:55 +0100

    Added eng translations

--- src/translations.ts
+++ src/translations.ts
@@ -4,16 +4,20 @@ export const translations: { [value: string]: DictionaryEntry } = {
   pageDescription: {
     namePl: `Nie wiesz gdzie potrzebna jest żywność, komu koce a gdzie zawieźć
      środki higieniczne? Zobacz aktualne zbiórki dla Ukrainy w twoim 
-     mieście.`
+     mieście.`,
+    nameEn: 'You have no idea where to donate food, who needs blankets and where to deliver hygienic supplies? Check the ongoing collections for ukrainian refugees in your city.'
   },
   shareActiveCollections: {
-    namePl: 'Udostępnij aktywne zbiórki'
+    namePl: 'Udostępnij aktywne zbiórki',
+    nameEn: 'Share all ongoing collections'
   },
   shareThisOrganizationCollection: {
-    namePl: 'Udostępnij zbiórkę tej organizacji'
+    namePl: 'Udostępnij zbiórkę tej organizacji',
+    nameEn: 'Share ongoing collections from this organisation'
   },
   aboutUsPageTitle: {
-    namePl: 'Copotrzebne.pl - O nas'
+    namePl: 'Copotrzebne.pl - O nas',
+    nameEn: 'Copotrzebne.pl - About us'
   },
   aboutUs: {
     namePl: `Pomagamy pomagać - łączymy miejsca oferujące pomoc rzeczową, noclegi i
@@ -21,74 +25,98 @@ export const translations: { [value: string]: DictionaryEntry } = {
       pomocy wspierać. Ułatwiamy lokalizację zbiórek i pomagamy koordynować
       zaspokajanie podstawowych potrzeb. Dzięki nam dowiesz się, co jest
       aktualnie najbardziej potrzebne w twojej lokalizacji oraz gdzie i kiedy
-      można dane rzeczy dostarczyć.`
+      można dane rzeczy dostarczyć.`,
+    nameEn: 'We are helping others help by connecting charity aid places and foundations, providing support to refugees from Ukraine - with people who would like to donate goods or items. We make it easier to locate the ongoing collections and coordinate demands for given supply. Thanks to us, you will know what is most needed in the aid center in your location and where and what you can donate to help.'
+
   },
   contactDetails: {
     namePl: `Chcesz dodać zbiórkę w twojej organizacji?
-      Skontaktuj się z nami:`
+      Skontaktuj się z nami:`,
+    nameEn: `Do you want to add collections in your organisation here?
+      Get in touch with us:`
   },
   chooseCurrentDemands: {
-    namePl: 'Wybierz aktualne potrzeby'
+    namePl: 'Wybierz aktualne potrzeby',
+    nameEn: 'Choose your current needs'
   },
   logIn: {
-    namePl: 'Zaloguj się'
+    namePl: 'Zaloguj się',
+    nameEN: 'Login'
   },
   email: {
-    namePl: 'Email'
+    namePl: 'Email',
+    nameEn: 'Email'
   },
   password: {
-    namePl: 'Hasło'
+    namePl: 'Hasło',
+    nameEn: 'Password'
   },
   name: {
-    namePl: 'Nazwa'
+    namePl: 'Nazwa',
+    nameEn: 'Name'
   },
   city: {
-    namePl: 'Miasto'
+    namePl: 'Miasto',
+    nameEn: 'City'
   },
   street: {
-    namePl: 'Ulica'
+    namePl: 'Ulica',
+    nameEn: 'Street'
   },
   buildingNumber: {
-    namePl: 'Numer budynku'
+    namePl: 'Numer budynku',
+    nameEn: 'Building no.'
   },
   apartmentNumber: {
-    namePl: 'Numer lokalu'
+    namePl: 'Numer lokalu',
+    nameEn: 'Flat'
   },
   comment: {
-    namePl: 'Komentarz'
+    namePl: 'Komentarz',
+    nameEn: 'Comment'
   },
   phone: {
-    namePl: 'Numer telefonu'
+    namePl: 'Numer telefonu',
+    nameEn: 'Contact person'
   },
   latitude: {
-    namePl: 'Szerokość geograficzna'
+    namePl: 'Szerokość geograficzna',
+    nameEn: 'Latitude'
   },
   longitude: {
-    namePl: 'Długość geograficzna'
+    namePl: 'Długość geograficzna',
+    nameEn: 'Longitude'
   },
   save: {
-    namePl: 'Zapisz'
+    namePl: 'Zapisz',
+    nameEn: 'Save'
   },
   pageNotFound: {
     namePl: 'Nie znaleziono strony',
     nameEn: 'Page not found'
   },
   addPlace: {
-    namePl: 'Dodaj miejsce'
+    namePl: 'Dodaj miejsce',
+    nameEn: 'Add place'
   },
   noDemandsReported: {
-    namePl: 'Aktualne nie masz zgłoszonych żadnych potrzeb'
+    namePl: 'Aktualne nie masz zgłoszonych żadnych potrzeb',
+    nameEn: 'You have no ongoing collections'
   },
   addDemands: {
-    namePl: 'Dodaj potrzeby'
+    namePl: 'Dodaj potrzeby',
+    nameEn: 'Add demands'
   },
   finishCollection: {
-    namePl: 'Zakończ zbiórkę'
+    namePl: 'Zakończ zbiórkę',
+    nameEn: 'Finish current collection'
   },
   lastUpdate: {
-    namePl: 'Ostatnia aktualizacja:'
+    namePl: 'Ostatnia aktualizacja:',
+    nameEn: 'Last modified at:'
   },
   demandsList: {
-    namePl: 'Lista potrzeb'
+    namePl: 'Lista potrzeb',
+    nameEn: 'Demands list'
   }
 }
