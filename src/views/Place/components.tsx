b57037b5c37492d24b9777d5c5636d2e9cd14dd1
Wojciech Bakłażec <wojciech.baklazec@schibsted.com>
2022-05-03 22:14:12 +0200

    feat: place faq texts

--- src/views/Place/components.tsx
+++ src/views/Place/components.tsx
@@ -1,6 +1,11 @@
 import { ReactNode, useState } from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { ReactComponent as ArrowIcon } from 'assets/down-arrow.svg'
+import { breakpoint } from 'themes/breakpoints'
+import FacebookShareButton from 'components/FacebookShareButton'
+import mapPlaceholderUrl from 'assets/map-background.svg'
+import Dialog from 'components/Dialog'
+import PageTitle from 'components/PageTitle'
 
 const CollapsableComponent = ({
   className,
@@ -65,6 +70,9 @@ const SectionContent = styled.div`
   flex-direction: column;
   width: 100%;
   padding: 0.7rem;
+  a {
+    color: ${({ theme }) => theme.colors.blue};
+  }
 `
 
 const ToggleIcon = styled.div<{ opened: boolean }>`
@@ -78,4 +86,341 @@ const ToggleIcon = styled.div<{ opened: boolean }>`
     `
     margin-top: -2px;
   `}
+`
+
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  min-height: calc(100vh - ${({ theme }) => theme.dimensions.headerHeight});
+  flex-direction: column;
+  padding-bottom: 4.8rem;
+  ${breakpoint.sm`
+    padding-bottom: 0;
+  `}
+`
+
+export const PlaceDetails = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.grey100};
+  padding: 1rem;
+  padding-bottom: 0.4rem;
+  ${breakpoint.sm`
+    max-width: 450px;
+    margin: 0 auto;
+  `}
+`
+export const PlaceDetailsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+`
+
+export const PlaceDescription = styled.p`
+  display: inline-block;
+  text-align: left;
+  line-height: 1.4;
+  width: 100%;
+  color: ${({ theme }) => theme.colors.grey500};
+  font-size: 0.8rem;
+  font-weight: 400;
+  overflow-wrap: break-word;
+  margin: 0.4rem 0;
+  a {
+    color: ${({ theme }) => theme.colors.blue};
+  }
+`
+
+export const PlaceAddressWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+export const PlaceAddress = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  span {
+    display: inline-flex;
+    align-items: center;
+    color: ${({ theme }) => theme.colors.grey800};
+    font-size: 0.85rem;
+    font-weight: 400;
+    margin: 0.4rem 0;
+  }
+`
+
+export const DetailsRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+`
+
+export const LastUpdate = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+
+  span {
+    color: #8d99b2;
+    font-size: 0.7rem;
+    font-weight: 400;
+    margin: 0.1rem 0;
+  }
+
+  h3 {
+    color: #8d99b2;
+    font-size: 0.78rem;
+    font-weight: 500;
+    margin: 0.1rem 0;
+  }
+`
+
+export const BankAccount = styled.div`
+  font-size: 0.8rem;
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.grey800};
+  a {
+    color: ${({ theme }) => theme.colors.blue};
+  }
+  span {
+    display: inline-block;
+    font-size: 0.9rem;
+    margin: 0;
+    line-height: 1.2;
+  }
+`
+
+export const DemandsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 1.2rem 3.2rem;
+  width: 100%;
+  ${breakpoint.sm`
+    max-width: 450px;
+    margin: 0 auto;
+  `}
+`
+
+export const DemandsListTitle = styled.h4`
+  display: flex;
+  width: 100%;
+  padding: 1rem 0;
+  font-size: 1.6rem;
+  font-weight: 700;
+  color: #333333;
+`
+
+export const DemandsList = styled.ol`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 0 1.2rem;
+  margin: 0;
+`
+
+export const DemandComponent = styled.li`
+  margin: 0.6rem;
+  padding-bottom: 0.6rem;
+  border-bottom: 1px solid #999;
+
+  & > span {
+    color: #999;
+  }
+
+  & > b {
+    color: #333;
+    font-weight: 600;
+  }
+`
+
+export const DemandInfo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & > span {
+    color: #333333;
+  }
+`
+
+export const DemandComment = styled.div`
+  margin-top: 0.2rem;
+  color: #999999;
+`
+
+export const StyledFacebookButton = styled(FacebookShareButton)`
+  max-width: 90%;
+  cursor: pointer;
+  z-index: 130;
+  position: fixed;
+  bottom: 2rem;
+  left: 50%;
+  transform: translateX(-50%);
+  box-shadow: ${({ theme }) => theme.boxShadows.medium};
+  ${breakpoint.sm`
+    bottom: 2rem;
+    right: 1rem;
+    left: unset;
+    transform: unset;
+    max-width: 35%;
+  `}
+`
+
+export const CategoryHeader = styled.span`
+  font-weight: bold;
+  display: flex;
+  width: calc(100% + 48px);
+  font-size: 1.05rem;
+  padding: 0.7rem;
+  color: #333333;
+  border-radius: 12px;
+  background-color: #0076ff1f;
+  margin: 0.4rem -24px;
+`
+
+export const PlaceNameLabel = styled.span`
+  display: inline-block;
+  width: 100%;
+  font-size: 0.8rem;
+  color: ${({ theme }) => theme.colors.grey500};
+`
+
+export const StyledPageTitle = styled(PageTitle)`
+  padding-top: 0;
+  flex-direction: column;
+  padding: 0 0 0.6rem 0;
+`
+
+export const ShowOnMapButton = styled.a`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  width: 100%;
+  height: 34px;
+  padding: 0.8rem 1.8rem;
+  outline: none;
+  background-color: transparent;
+  background-image: url(${mapPlaceholderUrl});
+  background-position: center center;
+  background-size: cover;
+  color: black;
+  font-size: 0.85rem;
+  font-weight: 600;
+  border-top: 2px solid white;
+  ${breakpoint.sm`
+    max-width: 450px;
+    margin: 0 auto;
+  `}
+`
+
+export const UrgentDemandsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`
+
+export const UrgentDemandsTitle = styled.div`
+  margin-top: 0.4rem;
+  display: inline-block;
+  width: 100%;
+  font-size: 0.7rem;
+  line-height: 1.2;
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.blue};
+`
+
+export const UrgentDemandsList = styled.div`
+  display: flex;
+  width: 100%;
+  overflow-x: auto;
+  padding: 0.4rem 0 0.8rem 0;
+  ${breakpoint.sm`
+    overflow-x: hidden;
+  `}
+`
+
+export const UrgentDemand = styled.div`
+  display: flex;
+  padding: 0.2rem 0.4rem;
+  background-color: #0076ff2b;
+  border-radius: 8px;
+  flex-shrink: 0;
+  font-size: 0.75rem;
+  margin-right: 0.4rem;
+`
+
+export const OrganisationLink = styled.a`
+  display: inline-block;
+  padding: 0 0.4rem;
+  svg {
+    fill: ${({ theme }) => theme.colors.gray800};
+  }
+`
+
+export const PlaceName = styled.span`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+export const gradient = keyframes`
+  0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+`
+
+export const HowCanIHelpButton = styled.button`
+  border: none;
+  outline: none;
+  padding: 0.8rem 1.8rem;
+  background-color: ${({ theme }) => theme.colors.blue};
+  color: white;
+  border-radius: 10px;
+  height: 38px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-size: 0.85rem;
+  font-weight: 500;
+  position: relative;
+  margin-top: 0.8rem;
+  margin-bottom: 0.8rem;
+  animation: ${gradient} 20s ease infinite;
+  background: linear-gradient(
+      -45deg,
+      rgb(107, 25, 216),
+      rgb(7, 210, 179),
+      ${({ theme }) => theme.colors.blue}
+    )
+    0% 0%/600% 600%;
+`
+
+export const FaqDialog = styled(Dialog)`
+  & > div {
+    & > div {
+      padding: 4.2rem 1.2rem 0;
+      overflow-y: auto;
+      ${breakpoint.sm`
+        width: 450px;
+        max-height: 80%;
+        box-shadow: ${({ theme }) => theme.boxShadows.medium};
+      `}
+    }
+  }
 `
