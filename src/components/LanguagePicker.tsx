ed7e85f8867d19c0e5c8f4410586059fea560a54
michal-grabowski <michal.grabowski@schibsted.com>
2022-05-06 09:48:48 +0200

    Add languages picker dropdown

--- src/components/LanguagePicker.tsx
+++ src/components/LanguagePicker.tsx
@@ -8,39 +8,73 @@ export default () => {
   const { language, changeLanguage } = useUserContext()
 
   return (
-    <>
-      <Language
-        selected={language === 'pl'}
-        onClick={() => changeLanguage('pl')}
-      >
-        <PolandFlag />
+    <Languages>
+      <Language selected>
+        {language === 'pl' && <PolandFlag />}
+        {language === 'ua' && <UkraineFlag />}
+        {language === 'en' && <EnglandFlag />}
       </Language>
-      <Language
-        selected={language === 'ua'}
-        onClick={() => changeLanguage('ua')}
-      >
-        <UkraineFlag />
-      </Language>
-      <Language
-        selected={language === 'en'}
-        onClick={() => changeLanguage('en')}
-      >
-        <EnglandFlag />
-      </Language>
-    </>
+      <Dropdown>
+        {language !== 'pl' && (
+          <Language onClick={() => changeLanguage('pl')}>
+            <PolandFlag />
+          </Language>
+        )}
+        {language !== 'ua' && (
+          <Language onClick={() => changeLanguage('ua')}>
+            <UkraineFlag />
+          </Language>
+        )}
+        {language !== 'en' && (
+          <Language onClick={() => changeLanguage('en')}>
+            <EnglandFlag />
+          </Language>
+        )}
+      </Dropdown>
+    </Languages>
   )
 }
 
-const Language = styled.div<{ selected: boolean }>`
-  margin-left: 0.6rem;
+const Dropdown = styled.div`
+  background: white;
+  position: absolute;
+  top: 40px;
+  left: 0;
+  box-shadow: ${({ theme }) => theme.boxShadows.medium};
+  border-radius: 4px;
+  transition: 0.3s;
+  max-height: 0;
+  opacity: 0;
+  overflow: hidden;
+`
+
+const Languages = styled.div`
+  position: relative;
+
+  &:hover {
+    ${Dropdown} {
+      max-height: 200px;
+      opacity: 1;
+    }
+  }
+`
+
+const Language = styled.div<{ selected?: boolean }>`
+  margin: 0.6rem;
   cursor: pointer;
   display: flex;
   align-items: center;
-  transform: scale(1);
-  transition: 0.3s;
+  height: 25px;
 
-  & > svg {
-    width: 2.2rem;
-    box-shadow: 0 0 3px #b0b0b0;
-  }
+  ${({ selected }) =>
+    selected &&
+    `
+      cursor: auto;
+    `}
+}
+
+& > svg {
+  width: 2.2rem;
+  box-shadow: 0 0 3px #b0b0b0;
+}
 `
