92b66b96f1d97cef362e2fee8c6fa59994a6816f
michal-grabowski <michal.grabowski@schibsted.com>
2022-05-12 16:35:56 +0200

    Add some copies to create better context of changing language

--- src/components/LanguagePicker.tsx
+++ src/components/LanguagePicker.tsx
@@ -3,6 +3,7 @@ import { ReactComponent as PolandFlag } from '../assets/poland-flag-square.svg'
 import { ReactComponent as EnglandFlag } from '../assets/england-flag-square.svg'
 import { ReactComponent as UkraineFlag } from '../assets/ukraine-flag-square.svg'
 import { useUserContext } from '../contexts/userContext'
+import TranslatedText from './TranslatedText'
 
 export default () => {
   const { language, changeLanguage } = useUserContext()
@@ -13,21 +14,27 @@ export default () => {
         {language === 'pl' && <PolandFlag />}
         {language === 'ua' && <UkraineFlag />}
         {language === 'en' && <EnglandFlag />}
+        <ChangeLanguageButtonText>
+          (<TranslatedText value="changeLanguage" />)
+        </ChangeLanguageButtonText>
       </Language>
       <Dropdown>
         {language !== 'pl' && (
           <Language onClick={() => changeLanguage('pl')}>
             <PolandFlag />
+            <TranslatedText value="polish" />
           </Language>
         )}
         {language !== 'ua' && (
           <Language onClick={() => changeLanguage('ua')}>
             <UkraineFlag />
+            <TranslatedText value="ukrainian" />
           </Language>
         )}
         {language !== 'en' && (
           <Language onClick={() => changeLanguage('en')}>
             <EnglandFlag />
+            <TranslatedText value="english" />
           </Language>
         )}
       </Dropdown>
@@ -39,13 +46,20 @@ const Dropdown = styled.div`
   background: white;
   position: absolute;
   top: 40px;
-  left: 0;
+  left: 50%;
+  transform: translateX(-50%);
   box-shadow: ${({ theme }) => theme.boxShadows.medium};
   border-radius: 4px;
   transition: 0.3s;
   max-height: 0;
   opacity: 0;
   overflow: hidden;
+  padding: 0.4rem;
+`
+
+const ChangeLanguageButtonText = styled.p`
+  text-decoration: underline;
+  cursor: pointer;
 `
 
 const Languages = styled.div`
@@ -76,5 +90,6 @@ const Language = styled.div<{ selected?: boolean }>`
 & > svg {
   width: 2.2rem;
   box-shadow: 0 0 3px #b0b0b0;
+  margin-right: 0.6rem;
 }
 `
